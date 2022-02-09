dea5a77667fd165b2de46a8e91467e76f7347c5f
unknown <lprieu@gamingcampus.local>
2022-02-09 20:55:19 +0100

    ok c finit chacal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// bouton show video
+
 document.getElementById("out")
         .addEventListener("click", function() {
   document.getElementById("out_hidden").hidden = true;
@@ -24,4 +26,24 @@ function showSlides(n, no) {
      x[i].style.display = "none";
   }
   x[slideIndex[no]-1].style.display = "block";
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
